d560b87f7a0f4cc3ae8dfa8d6fef83f9b9dcf22d
vtcong21 <vtcong21@vp.fitus.edu.vn>
2023-07-28 19:32:19 +0700

    tạo model order và hệ thống thanh toán, chỉnh sửa các model cũ

--- models/paymentAccount.js
+++ models/paymentAccount.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const paymentAccountSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    balance: { type: Number, default: 0 }
+    balance: { type: Number, default: 0 },
+    paymentHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }]
 });
 
 const PaymentAccount = mongoose.model('PaymentAccount', paymentAccountSchema);
