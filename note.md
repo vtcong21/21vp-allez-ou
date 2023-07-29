c323ca8f92fc1bd9e18e30b00fb7ba291b8ea53b
lizly15 <thienly72752800@gmail.com>
2023-07-29 08:57:55 +0700

    .

---
+++ note.md
@@ -0,0 +1,9 @@
+push 
+1. git add .
+2. git commit -m "."
+3. git push origin vhtly21
+
+
+
+git pull
+xong lap lai buoc tren de push
--- wanli-web2/assets/js/script.js
+++ wanli-web2/assets/js/script.js
@@ -21,10 +21,95 @@ function removeHeaderBackground() {
     }
 }
 
+let sortButtonState1 = 0;
+let sortButtonState2 = 0;
+
 let currentPage = 0;
 let contentPerPage = 10;
 let limPage = 0;
 var listTravelTour = [];
+
+function compareEvents(event1, event2) {
+  const date1 = new Date(event1.detail.date);
+  const date2 = new Date(event2.detail.date);
+  const time1 = event1.detail.time;
+  const time2 = event2.detail.time;
+
+  if (date1 < date2) {
+    return -1;
+  } else if (date1 > date2) {
+    return 1;
+  } else {
+    if (time1 < time2) {
+      return -1;
+    } else if (time1 > time2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+function sortButtonActive1(button) {
+	sortButtonState2 = 0;
+	sortButtonState1 = (sortButtonState1 + 1) % 3;
+	var images = document.querySelectorAll("#travel .listtour .tour-sort .sort-block img");
+	for(var i = 0; i < images.length; i++) {
+		images[i].src = "./assets/img/up-arrow.svg";
+	}
+
+	var image = button.querySelector('img');
+	if(sortButtonState1 == 2) image.src = "./assets/img/down-arrow.svg";
+	else image.src = "./assets/img/up-arrow.svg";
+
+	var sortButtons = document.querySelectorAll("#travel .listtour .tour-sort .sort-block");
+	for(var i = 0; i < sortButtons.length; i++) {
+		sortButtons[i].style.backgroundColor = '#6E6A8E';
+	}
+
+	if(sortButtonState1 == 0) button.style.backgroundColor = '#6E6A8E';
+	else button.style.backgroundColor = '#F14868';
+
+	if(sortButtonState1 == 1) {
+		listTravelTour.sort(compareEvents);
+		console.log(listTravelTour);
+	}
+	else if(sortButtonState1 == 2) {
+		listTravelTour.sort(compareEvents);
+		listTravelTour.reverse();
+		console.log(listTravelTour);
+	}
+	generateTravelTour(listTravelTour);
+}
+
+function sortButtonActive2(button) {
+	sortButtonState1 = 0;
+	sortButtonState2 = (sortButtonState2 + 1) % 3;
+	var images = document.querySelectorAll("#travel .listtour .tour-sort .sort-block img");
+	for(var i = 0; i < images.length; i++) {
+		images[i].src = "./assets/img/up-arrow.svg";
+	}
+
+	var image = button.querySelector('img');
+	if(sortButtonState2 == 2) image.src = "./assets/img/down-arrow.svg";
+	else image.src = "./assets/img/up-arrow.svg";
+
+	var sortButtons = document.querySelectorAll("#travel .listtour .tour-sort .sort-block");
+	for(var i = 0; i < sortButtons.length; i++) {
+		sortButtons[i].style.backgroundColor = '#6E6A8E';
+	}
+
+	if(sortButtonState2 == 0) button.style.backgroundColor = '#6E6A8E';
+	else button.style.backgroundColor = '#F14868';
+
+	if(sortButtonState2 == 1) {
+		listTravelTour.sort((travel1, travel2) => travel1.detail.price - travel2.detail.price);
+	}
+	else if(sortButtonState2 == 2) {
+		listTravelTour.sort((travel1, travel2) => travel2.detail.price - travel1.detail.price);
+	}
+	generateTravelTour(listTravelTour);
+}
 
 function generateTravelTour(data){
 	data = data.slice(currentPage * contentPerPage, currentPage * contentPerPage + contentPerPage);
@@ -38,6 +123,12 @@ function generateTravelTour(data){
 	const pageInfoVal = (currentPage + 1).toString() + " / " + limPage.toString();
 
 	pageInfo.innerHTML = pageInfoVal;
+
+	var listTravels = document.querySelectorAll('#travel .travel-content .listtour .row2 h2');
+	for(var i = 0; i < listTravels.length; i++) {
+		var price = listTravels[i].innerHTML;
+		listTravels[i].innerHTML = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "đ";
+	}
 	// list.find('.col').on('click', function (e) {
 	// 	e.preventDefault();
 	// 	var tourIndex = $(this).data('index');
