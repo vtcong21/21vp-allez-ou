285c4cce20033d19af2554f8b1f1080aa0a147a5
phivu3125 <phivu3125@gmail.com>
2023-07-30 11:37:27 +0700

    save

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,9 @@
-function copyTolipBoard(text) {
-    console.log('Click');
+function copyPhone(text) {
     navigator.clipboard.writeText(text);
-    
-    // Alert the copied text
     alert("Đã copy số điện thoại vào clip board");
-  }
+}
+
+function copyMail(text) {
+    navigator.clipboard.writeText(text);
+    alert("Đã copy emai vào clip board");
+}
