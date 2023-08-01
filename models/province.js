501470b537790f45ff2496789c0086e647fa5753
vtcong21 <vtcong21@vp.fitus.edu.vn>
2023-08-01 14:12:23 +0700

    add collections

--- models/province.js
+++ models/province.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const provinceSchema = new mongoose.Schema({
-    code: {type: String, require : true, unique : true},
-    name: {type: String}
+    code: { type: String, required: true, unique: true },
+    name: { type: String }
 });
 
-
 const Province = mongoose.model('Province', provinceSchema);
+
 module.exports = Province;
