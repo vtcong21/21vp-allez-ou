0f3ec90b48bc05317a0b45f10db77d7582e4527a
phivu3125 <phivu3125@gmail.com>
2023-08-06 20:14:50 +0700

    fixed bug navbar and tourInfo

---
+++ public/js/home.js
@@ -0,0 +1,16 @@
+// async function getDataFromAPI() {
+//     try {
+//       const response = await axios.get('provinces');
+
+//       if (response.status === 200) {
+//         console.log(response.data)
+//       } else {
+//         throw new Error('Lỗi khi gửi yêu cầu đến API');
+//       }
+//     } catch (error) {
+//       console.error('Đã xảy ra lỗi:', error.message);
+//       throw error; 
+//     }
+// }
+
+// getDataFromAPI();
