0139cc1f12d050aeab26675392da5eef9ab9a088
tbnguyen21vp <tbnguyen21@vp.fitus.edu.vn>
2023-08-11 01:49:44 +0700

    10/8 done trang dangky, trang bai dang admin da chinh css chi con laithanh trang thai

--- webServer/public/js/baidang.js
+++ webServer/public/js/baidang.js
@@ -1,19 +1,4 @@
-//  // Lấy giá trị float-end
-//  var floatEnd = document.getElementById('floatValue').textContent;
-    
-//  // Lấy giá trị floatEnd và chuyển đổi thành phần số
-//  var percentage = parseFloat(floatEnd);
- 
-//  // Lấy giá trị của aria-valuemax
-//  var maxValue = parseFloat(document.getElementById('progressBar').getAttribute('aria-valuemax'));
- 
-//  // Tính toán giá trị width dựa trên giá trị floatEnd và maxValue
-//  var widthValue = (percentage / maxValue) * 100;
- 
-//  // Set giá trị width trong style của progress-bar
-//  var progressBar = document.getElementById('progressBar');
-//  progressBar.style.width = widthValue + "%";
-// Khai báo biến global để lưu trữ danh sách tour và số trang hiện tại
+
 let tourDataList = [];
 let currentPage = 1;
 
@@ -44,7 +29,28 @@ function renderTourPage(page) {
     // Tạo thẻ div cho từng tour trên trang hiện tại
     currentTourDataList.forEach(tourData => {
         const tourCode = tourData.code;
+        const tourDate = tourData.date;
+        const slots = tourData.slots;
         const remainSlots = tourData.remainSlots;
+        const tourName = tourData.name;
+const maxLength = 96;
+// function calculateProgressBarWidth(slots, remainSlots) {
+//     const usedSlots = slots - remainSlots;
+//     const progressbar = (usedSlots / slots) * 100;
+//     return progressbar;
+//   }
+//   const progressbarwidth = calculateProgressBarWidth(slots,remainSlots); 
+//     const progressbaredit = document.getElementsByClassName(`progress-bar`);
+//     progressbaredit.style.width = progressbarwidth + "%";  
+
+let truncatedName = tourName;
+if (tourName.length > maxLength) {
+  truncatedName = tourName.substring(0, maxLength - 10) + "...";
+}
+const formattedDate = new Date(tourDate).toLocaleDateString('en-GB');
+const dateParts = formattedDate.split('/');
+const departureDate = `${dateParts[0]}/${dateParts[1]}/${dateParts[2]}`;
+const tourDate1 = departureDate;
 
         // Tạo thẻ div cho tour
         const tourDiv = document.createElement('div');
@@ -52,31 +58,45 @@ function renderTourPage(page) {
 
         // Tạo nội dung cho thẻ div
         tourDiv.innerHTML = `
-            <div class="pageContainer">
-                <div class="card d-block">
-                    <div class="card-body">
-                        <div id="tour-title-${tourCode}">
-                            <div style="text-align: center;">
-                                <label for="information-tour" style="text-decoration: underline;">Thông tin chuyến đi</label>
-                            </div>
-                            <div class="name-tour">
-                                ${tourData.name}
-                            </div>
-                        </div>
+        <div class="pageContainer">
+    <div class="card d-block">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-9 text-left" id="name-tour">
+                    <div id="tour-title-${tourCode}">
+                        <div class="name-tour">${truncatedName}</div>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item p-3">
-                            <h3 class="mb-2 fw-bold">Còn trống
-                                <span id="floatValue-${tourCode}" class="float-end">${remainSlots}</span>
-                            </h3>
-                            <div class="progress progress-sm">
-                                <div id="progressBar-${tourCode}" class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="10">
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                </div>
+                <div class="col-3 text-right">
+                    <button id="tacvu" type="button">&#8230;</button>
                 </div>
             </div>
+            <div class="row" id="code-tour">
+                <div class="col-11 text-left">
+                    <div id="tour-title-${tourCode}">
+                        <div class="code-tour">${tourCode}</div>
+                        <div class="code-tour">${tourDate1}</div>
+                    </div>
+                </div>
+                <div class="col-1 text-right">
+                </div>
+            </div>
+
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item p-3">
+                <h3 class="mb-2 fw-bold">
+                    Còn trống
+                    <span id="floatValue-${tourCode}" class="float-end">${remainSlots}</span>
+                </h3>
+                <div class="progress progress-sm">
+                    <div id="progressBar-${tourCode}" class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="10"></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+
         `;
 
         // Thêm thẻ div vào container
@@ -85,6 +105,7 @@ function renderTourPage(page) {
 
     // Tạo phân trang
     renderPagination();
+    
 }
 
 function renderPagination() {
