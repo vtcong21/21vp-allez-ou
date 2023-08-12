3129790f433a0d35de1e9d62f61c952b3f0f6ad3
phivu3125 <phivu3125@gmail.com>
2023-08-12 07:07:44 +0700

    save

--- webServer/public/js/dashboard.js
+++ webServer/public/js/dashboard.js
@@ -1,51 +1,83 @@
-(function () {
-    'use strict'
+// Hàm để cập nhật dữ liệu biểu đồ
+function updateChartWithData(data) {
+  var ctx = document.getElementById('myChart').getContext('2d');
   
-    feather.replace({ 'aria-hidden': 'true' })
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
+  var myChart = new Chart(ctx, {
       type: 'line',
       data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
+          labels: [
+            changeDateToString(data[0].date),
+            changeDateToString(data[1].date),
+            changeDateToString(data[2].date),
+            changeDateToString(data[3].date),
+            changeDateToString(data[4].date),
+            changeDateToString(data[5].date),
+            changeDateToString(data[6].date),
           ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
+          datasets: [{
+              data: [
+                data[0].revenue,
+                data[1].revenue,
+                data[2].revenue,
+                data[3].revenue,
+                data[4].revenue,
+                data[5].revenue,
+                data[6].revenue,
+              ],
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#007bff',
+              borderWidth: 4,
+              pointBackgroundColor: '#007bff'
+          }]
       },
       options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false,
-            }
-          }]
-        },
-        legend: {
-          display: false
-        }
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: false,
+                  }
+              }]
+          },
+          legend: {
+              display: false
+          }
       }
-    })
-  })()
+  });
+}
+
+// Hàm gọi API và cập nhật biểu đồ
+
+function changeDateToString(currentTimeString) {
+  var currentTime = new Date(currentTimeString); // Chuyển chuỗi thành đối tượng Date
+  var day = currentTime.getDate();
+  var month = currentTime.getMonth() + 1;
+  var year = currentTime.getFullYear();
+
+  if (day.toString().length === 1) {
+      day = "0" + day.toString();
+  }
+  if (month.toString().length === 1) {
+      month = "0" + month.toString();
+  }
+
+  return day + "/" + month + "/" + year;
+}
+
+async function postToGetDashboard() {
+  try {
+      const response = await axios.post('/dashboard/getRevenueLast7Days');
+      let dashboards = response.data.last7DaysRevenue;
+    //   dashboards = dashboards.map((dashboard) => {
+    //     const formattedDate = changeDateToString(dashboard.date);
+    //     return { ...dashboard, date: formattedDate};
+    // });
+      // Gọi hàm cập nhật biểu đồ với dữ liệu từ API
+      updateChartWithData(dashboards);
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+// Gọi hàm để lấy dữ liệu từ API và cập nhật biểu đồ
+postToGetDashboard();
