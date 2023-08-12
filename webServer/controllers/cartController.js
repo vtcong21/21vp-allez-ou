fe8ea7dbcc72d2863a792944ced0c6a6f754dc45
xuanuyenvu <xuanuyenvu22@gmail.com>
2023-08-12 18:46:09 +0700

    render cart page

--- webServer/controllers/cartController.js
+++ webServer/controllers/cartController.js
@@ -2,6 +2,30 @@ const User = require('../models/user');
 const Tour = require('../models/tour'); 
 const Item = require('../models/item'); 
 
+function convertGenderToVietnamese(gender) {
+    if (gender === "Male") {
+        return "Nam";
+    } else if (gender === "Female") {
+        return "Nữ";
+    } else {
+        return gender;
+    }
+}
+function changeDateToString(currentTime) {
+    let day = currentTime.getDate();
+    let month = currentTime.getMonth() + 1;
+    let year = currentTime.getFullYear();
+  
+    if (day.toString().length === 1) {
+      day = "0" + day.toString();
+    }
+    if (month.toString().length === 1) {
+      month = "0" + month.toString();
+    }
+  
+    return day + "/" + month + "/" + year;
+}
+
 const addNewItem = async (req, res) => {
     try {
         const userId = req.userId;
@@ -41,16 +65,31 @@ const getCartPage = async (req, res) => {
             return res.status(404).send('There are no products in the shopping cart');
         }
 
-        const cartItems = await Promise.all(userCart.cart.map(async (item) => {
-            const tour = await Tour.findOne({ code: item.tourCode });
+        let cartItems = await Promise.all(userCart.cart.map(async (item) => {
+            let tour = await Tour.findOne({ code: item.tourCode });
+            const formattedStartDate = changeDateToString(tour.date);
+            tour = { ...tour.toObject(), date: formattedStartDate };
+
             return {
+                imgURL: tour.cardImgUrl,
                 name: tour.name,
                 date: tour.date,
                 price: tour.price,
             };
         }));
 
-        res.render('cart', { cartItems });
+
+        let user = await User.findById(req.userId).select('fullName email dateOfBirth phoneNumber gender');
+
+        if (user) {
+          const formattedDateOfBirth = changeDateToString(user.dateOfBirth);
+          const formattedGender = convertGenderToVietnamese(user.gender);
+          user = { ...user.toObject(), dateOfBirth: formattedDateOfBirth, gender: formattedGender };
+          res.render('cart', { user, cartItems });
+        } else {
+          // Nếu không có user, render view EJS với dữ liệu user là null
+          res.render('home', { user: null });
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal server error');
