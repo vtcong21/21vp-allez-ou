fc5be5a357c4de3f4562294da990363bfc79cfdf
xuanuyenvu <xuanuyenvu22@gmail.com>
2023-08-14 16:07:26 +0700

    gan phuong thuc GET cho nut Dat tour

--- webServer/public/js/cart.js
+++ webServer/public/js/cart.js
@@ -1,5 +1,4 @@
 async function deleteItem(itemId) {
-    
     try {
         console.log(itemId);
         const response = await axios.delete('/cart/deleteItem', {
@@ -14,5 +13,19 @@ async function deleteItem(itemId) {
       console.log('thất bại delete');
       console.error("Error:", error);
 
+    }
+}
+
+async function bookTourPage(itemId, tourCode) {
+    try {
+        const response = await axios.get('user/getOrderPage', {
+            data: { itemId: itemId, tourCode: tourCode }
+        });
+
+        if (response.status === 200) {
+            console.log(tourCode);
+        }
+    } catch (error) {
+        console.error("Error:", error);
     }
 }
