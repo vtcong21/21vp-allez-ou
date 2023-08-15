03e0f4013f83e20e4e368d1ec6b7d09946e334fe
xuanuyenvu <xuanuyenvu22@gmail.com>
2023-08-15 15:29:56 +0700

    modal success

--- webServer/public/js/orderHistory.js
+++ webServer/public/js/orderHistory.js
@@ -1,139 +1,92 @@
-// for (order of ordersSuccess){
-    // }
-    
-// function openSuccessModal(index) {
-//     console.log(index)
-//     console.log(ordersSuccess[index].tour.name);
-//     // const modalBody = document.getElementById("bookingSuccessModal");
+function changeDateToString(currentTimeString) {
+    var currentTime = new Date(currentTimeString); // Chuyển chuỗi thành đối tượng Date
+    var day = currentTime.getDate();
+    var month = currentTime.getMonth() + 1;
+    var year = currentTime.getFullYear();
 
-//     // modalBody.insertAdjacentHTML("beforeend",  fillInfo(index));
+    if (day.toString().length === 1) {
+        day = "0" + day.toString();
+    }
+    if (month.toString().length === 1) {
+        month = "0" + month.toString();
+    }
 
-//     const modal = new bootstrap.Modal(document.getElementById('bookingSuccessModal'));
-//     modal.show();
-// }
-
-const myModal = new bootstrap.Modal(document.getElementById('myModal'));
-
-function openModal() {
-  myModal.show();
+    return day + "/" + month + "/" + year;
 }
 
-function fillInfo(i) {
-    return`
-    <section class="pt-4">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-xl-8 mx-auto">
-                <div class="card shadow">
-                    <!-- Image -->
-                    <img src="https://booking.webestica.com/assets/images/gallery/04.jpg" class="rounded-top" alt="" />
-                    <!-- Card body -->
-                    <ul class="card-body text-center p-4">
-                        <!-- Title -->
-                        <h1 class="card-title fss">🎊 Đặt thành công! 🎊</h1>
-                        <p class="lead mb-3">
-                            Cảm ơn bạn đã tin tưởng và lựa chọn dịch vụ của Allez òu,<br />
-                            dưới đây là thông tin vé của bạn
-                        </p>
-                        <!-- Second title -->
-                        <h5 class="text-primary mb-4" >${ordersSuccess[i].tour.name}</h5>
-                        <!-- List -->
-                        <div class="row justify-content-between text-start">
-                            <div class="col-12">
-                                <ul class="list-group list-group-borderless">
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-vr fa-fw me-2"></i>Mã tour: </span>
-                                        <span class="h6 fw-normal mb-0">NDSGN559-006-080823XE-V</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-person fa-fw me-2"></i>Khách đại diện: </span>
-                                        <span class="h6 fw-normal mb-0">Vũ Nguyễn Xuân Uyên</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-wallet2 fa-fw me-2"></i>Thanh toán: </span>
-                                        <span class="h6 fw-normal mb-0">Ngân hàng</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-currency-dollar fa-fw me-2"></i>Tổng tiền: </span>
-                                        <span class="h6 fw-normal mb-0">4,590,000₫</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-calendar fa-fw me-2"></i>Ngày xác nhận: </span>
-                                        <span class="h6 fw-normal mb-0">03/08/2023</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-calendar fa-fw me-2"></i>Ngày đi: </span>
-                                        <span class="h6 fw-normal mb-0">15/08/2023</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0"> <i class="bi bi-people fa-fw me-2"></i>Số khách: </span>
-                                        <span class="h6 fw-normal mb-0">3</span>
-                                    </li>
-                                </ul>
-                                <ul class="list-group list-group-borderless">
-                                    <p class="more-title">Thông tin liên hệ</p>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Email: </span>
-                                        <span class="h6 fw-normal mb-0">vnxuyen21@vp.fitus.edu.vn</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Số điện thoại: </span>
-                                        <span class="h6 fw-normal mb-0">093 883 7226</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Địa chỉ: </span>
-                                        <span class="h6 fw-normal mb-0">227 Nguyễn Văn Cừ Quận 5</span>
-                                    </li>
-                                </ul>
-                                <ul class="list-group list-group-borderless">
-                                    <p class="more-title">Khách hàng 1 <span class="badge rounded-pill bg-success"> NGƯỜI LỚN</span></p>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Họ tên: </span>
-                                        <span class="h6 fw-normal mb-0">vnxuyen21@vp.fitus.edu.vn</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Giới tính: </span>
-                                        <span class="h6 fw-normal mb-0">093 883 7226</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Ngày sinh: </span>
-                                        <span class="h6 fw-normal mb-0">227 Nguyễn Văn Cừ Quận 5</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Phòng riêng: </span>
-                                        <span class="h6 fw-normal mb-0">Có</span>
-                                    </li>
-                                </ul>
+function translateGenderToVietnamese(gender) {
+    switch (gender) {
+        case "Male":
+            return "Nam";
+        case "Female":
+            return "Nữ";
+        case "Undefined":
+            return "Không xác định";
+        default:
+            return "Giới tính không xác định";
+    }
+}
 
-                                <ul class="list-group list-group-borderless">
-                                    <p class="more-title">Khách hàng 2 <span class="badge rounded-pill bg-success"> TRẺ EM</span></p>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Họ tên: </span>
-                                        <span class="h6 fw-normal mb-0">vnxuyen21@vp.fitus.edu.vn</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Giới tính: </span>
-                                        <span class="h6 fw-normal mb-0">093 883 7226</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Ngày sinh: </span>
-                                        <span class="h6 fw-normal mb-0">227 Nguyễn Văn Cừ Quận 5</span>
-                                    </li>
-                                    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
-                                        <span class="mb-0">Phòng riêng: </span>
-                                        <span class="h6 fw-normal mb-0">Có</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+function translateTypeToVietnamese(type) {
+    switch (type) {
+        case "Adult":
+            return "Người lớn";
+        case "Teen":
+            return "Trẻ em";
+        case "Kid":
+            return "Trẻ nhỏ";
+        case "Baby":
+            return "Em bé";
+        default:
+            return "Không xác định";
+    }
+}
 
-                        <!-- Download button -->
-                        <a href="#" class="btn btn-primary mb-0"> <i class="bi bi-x-octagon"></i> Hủy chuyến </a>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    </section>
+const myModal = new bootstrap.Modal(document.getElementById('modal_1'));
+
+function openSuccessModal(i) {
+    console.log(ordersSuccess[i]);
+    
+    const modalElement = document.querySelector("#modal_1");
+    modalElement.querySelector("#order-name-tour").textContent = ordersSuccess[i].tour.name;
+    modalElement.querySelector("#order-code-tour").textContent = ordersSuccess[i].item.tourCode;
+    modalElement.querySelector("#order-representer-name").textContent = ordersSuccess[i].item.representer.name;
+    modalElement.querySelector("#order-total-price").textContent = ordersSuccess[i].item.totalPrice;
+    modalElement.querySelector("#order-start-date").textContent = ordersSuccess[i].tour.date;
+    modalElement.querySelector("#order-number-ticket").textContent = ordersSuccess[i].item.tickets.length;
+    modalElement.querySelector("#order-representer__email").textContent = ordersSuccess[i].item.representer.email;
+    modalElement.querySelector("#order-representer__phone").textContent = ordersSuccess[i].item.representer.phone;
+    modalElement.querySelector("#order-representer__address").textContent = ordersSuccess[i].item.representer.address;
+
+    const ticketsContainer = document.getElementById("order-tickets__container");
+    ticketsContainer.innerHTML = "";
+    console.log(ordersSuccess[i].item.tickets);
+    for (let j = 0; j < ordersSuccess[i].item.tickets.length; j++) {
+        let ticketRow = maketTicketRow(ordersSuccess[i].item.tickets[j], j + 1);
+        ticketsContainer.insertAdjacentHTML("beforeend", ticketRow);
+    }
+
+    myModal.show();
+}
+
+function maketTicketRow(ticket, index) {
+    return `
+    <ul class="list-group list-group-borderless">
+    <p class="more-title">Khách hàng ${index} <span class="badge rounded-pill bg-success"> ${translateTypeToVietnamese(ticket.type)}</span></p>
+    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
+        <span class="mb-0 h6-v2">Họ tên: </span>
+        <span class="h6-v2 fw-normal mb-0">${ticket.fullName}</span>
+    </li>
+    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
+        <span class="mb-0 h6-v2">Giới tính: </span>
+        <span class="h6-v2 fw-normal mb-0">${translateGenderToVietnamese(ticket.gender)}</span>
+    </li>
+    <li class="list-group-item d-sm-flex justify-content-between align-items-center">
+        <span class="mb-0 h6-v2">Ngày sinh: </span>
+        <span class="h6-v2 fw-normal mb-0">${changeDateToString(ticket.dateOfBirth)}</span>
+    </li>
+</ul>
     `;
 }
+
+
